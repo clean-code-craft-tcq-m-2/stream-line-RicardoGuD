ada0691aae033424fa5c972ee8777e09220e506c
jessicaGL <46303776+jessicaGL@users.noreply.github.com>
2023-03-09 23:13:04 -0600

    Create Receiver.h

---
+++ Receiver/Receiver.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void SimpleMovingAverage(int n, int array[]);
+int MaxValue(int n, int array[]);
+int MinValue(int n, int array[]);
+void printMaxMinBattery(int n, int Battery[]);
+void printMaxMinCurrent(int n, int Current[]);
